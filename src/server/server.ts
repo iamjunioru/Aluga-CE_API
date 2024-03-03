1719714cd0397fbfd05d00e1cbb80be9fbcc92c0
Junior <iamjunioru@gmail.com>
2024-03-03 20:56:08 -0300

    feat: public dirname

--- src/server/server.ts
+++ src/server/server.ts
@@ -6,7 +6,9 @@ import path from "path";
 const server = express();
 
 // Configuração para servir arquivos estáticos da pasta de uploads
+server.use(express.static(path.join(__dirname, 'public')));
 server.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 
 import './shared/services/TranslationsYup';
 
